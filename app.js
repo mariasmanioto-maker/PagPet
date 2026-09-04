440d79687aa89b1caf530978a327a2edf55ae788
mariasmanioto-maker <maria.smanioto@escola.pr.gov.br>
2026-09-04 11:15:59 -0300

    PARTE 5!!!!

---
+++ app.js
@@ -0,0 +1,8 @@
+
+
+ async function imagemApi(){
+    let respostaApi = await fetch('https://dog.ceo/api/breeds/image/random');
+    let dadosApi = await respostaApi.json();
+    document.querySelector(".img-dog").src = dadosApi.message;
+    console.log(dadosApi);
+}
