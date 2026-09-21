bc1cc05bab3ca03cd50e7ed9c676a03ff01c27cc
mariasmanioto-maker <maria.smanioto@escola.pr.gov.br>
2026-09-21 10:24:16 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -6,3 +6,15 @@
     document.querySelector(".img-dog").src = dadosApi.message;
     console.log(dadosApi);
 }
+
+// async --> assincrono 
+async function imagemApiCat(){
+    // espera chegar a resposta --> await
+    let respostaApi = await fetch("https://api.thecatapi.com/v1/images/search?limit=1", {
+  headers: { "x-api-key": "live_EIZqWUzPvGRkTFR8H4pSTlNuPpIO40ai6Rrplv0zL3l30L404fF7kZwd4IJZ0ORS" },
+});
+    let dadosApi = await respostaApi.json();
+    document.querySelector(".img-dog").src =dadosApi[0].url;
+    console.log(dadosApi[0].url);
+
+}
